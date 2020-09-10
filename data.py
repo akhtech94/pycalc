eedbcac80b056974426a9a85fe35a6d3070e0868
Akhil Ajit <akhtech94@gmail.com>
2020-09-10 16:13:52 +0530

    external module for data
    Co-authored-by: sreeshma739@gmail.com <sreeshma739@gmail.com>

---
+++ data.py
@@ -0,0 +1,1002 @@
+students = [
+	{
+		"rollno": 1,
+		"name": "Zachary",
+		"age": 20,
+		"sem": 6,
+		"branch": "cs",
+		"phone": "186-127-0899",
+		"address": "7767 Dictum St.",
+		"marks": 30
+	},
+	{
+		"rollno": 2,
+		"name": "Rudyard",
+		"age": 23,
+		"sem": 7,
+		"branch": "ec",
+		"phone": "976-713-9968",
+		"address": "Ap #472-4173 Lacus. St.",
+		"marks": 1
+	},
+	{
+		"rollno": 3,
+		"name": "Vera",
+		"age": 26,
+		"sem": 3,
+		"branch": "civil",
+		"phone": "838-925-9812",
+		"address": "P.O. Box 161, 9600 Vitae Rd.",
+		"marks": 91
+	},
+	{
+		"rollno": 4,
+		"name": "Jana",
+		"age": 19,
+		"sem": 7,
+		"branch": "civil",
+		"phone": "170-685-3434",
+		"address": "Ap #893-5542 Nibh Road",
+		"marks": 94
+	},
+	{
+		"rollno": 5,
+		"name": "Melinda",
+		"age": 19,
+		"sem": 8,
+		"branch": "ec",
+		"phone": "872-637-4103",
+		"address": "2680 A Rd.",
+		"marks": 49
+	},
+	{
+		"rollno": 6,
+		"name": "Hayley",
+		"age": 22,
+		"sem": 6,
+		"branch": "ec",
+		"phone": "785-890-6250",
+		"address": "P.O. Box 888, 7061 Varius Rd.",
+		"marks": 38
+	},
+	{
+		"rollno": 7,
+		"name": "Mufutau",
+		"age": 26,
+		"sem": 7,
+		"branch": "mech",
+		"phone": "907-116-9913",
+		"address": "Ap #605-9043 Quis Road",
+		"marks": 21
+	},
+	{
+		"rollno": 8,
+		"name": "Clementine",
+		"age": 18,
+		"sem": 2,
+		"branch": "civil",
+		"phone": "705-972-8665",
+		"address": "Ap #576-2746 Non Avenue",
+		"marks": 91
+	},
+	{
+		"rollno": 9,
+		"name": "Neil",
+		"age": 19,
+		"sem": 5,
+		"branch": "civil",
+		"phone": "505-301-4149",
+		"address": "383 Ut St.",
+		"marks": 74
+	},
+	{
+		"rollno": 10,
+		"name": "Skyler",
+		"age": 19,
+		"sem": 1,
+		"branch": "mech",
+		"phone": "585-963-7116",
+		"address": "P.O. Box 147, 3404 Curabitur St.",
+		"marks": 54
+	},
+	{
+		"rollno": 11,
+		"name": "Aaron",
+		"age": 20,
+		"sem": 6,
+		"branch": "civil",
+		"phone": "374-326-3127",
+		"address": "5446 Adipiscing Road",
+		"marks": 34
+	},
+	{
+		"rollno": 12,
+		"name": "Kirestin",
+		"age": 23,
+		"sem": 4,
+		"branch": "civil",
+		"phone": "802-615-3987",
+		"address": "3863 Blandit Street",
+		"marks": 29
+	},
+	{
+		"rollno": 13,
+		"name": "Lyle",
+		"age": 25,
+		"sem": 1,
+		"branch": "ec",
+		"phone": "766-936-4465",
+		"address": "Ap #422-4335 Ipsum St.",
+		"marks": 92
+	},
+	{
+		"rollno": 14,
+		"name": "Leo",
+		"age": 21,
+		"sem": 4,
+		"branch": "cs",
+		"phone": "446-840-2491",
+		"address": "P.O. Box 875, 1387 Donec Street",
+		"marks": 48
+	},
+	{
+		"rollno": 15,
+		"name": "Chantale",
+		"age": 20,
+		"sem": 1,
+		"branch": "cs",
+		"phone": "563-130-3771",
+		"address": "Ap #374-6655 Magna Rd.",
+		"marks": 35
+	},
+	{
+		"rollno": 16,
+		"name": "Farrah",
+		"age": 22,
+		"sem": 7,
+		"branch": "it",
+		"phone": "366-466-6803",
+		"address": "356-7660 Feugiat St.",
+		"marks": 49
+	},
+	{
+		"rollno": 17,
+		"name": "Anika",
+		"age": 21,
+		"sem": 3,
+		"branch": "cs",
+		"phone": "763-542-4631",
+		"address": "Ap #855-1511 Sollicitudin Street",
+		"marks": 86
+	},
+	{
+		"rollno": 18,
+		"name": "Calvin",
+		"age": 21,
+		"sem": 8,
+		"branch": "civil",
+		"phone": "284-453-0088",
+		"address": "6742 Risus Street",
+		"marks": 68
+	},
+	{
+		"rollno": 19,
+		"name": "Burke",
+		"age": 21,
+		"sem": 6,
+		"branch": "ec",
+		"phone": "778-489-5620",
+		"address": "P.O. Box 521, 3436 Egestas St.",
+		"marks": 72
+	},
+	{
+		"rollno": 20,
+		"name": "Raven",
+		"age": 19,
+		"sem": 2,
+		"branch": "it",
+		"phone": "419-124-5236",
+		"address": "765-6447 Imperdiet Ave",
+		"marks": 77
+	},
+	{
+		"rollno": 21,
+		"name": "Geraldine",
+		"age": 25,
+		"sem": 7,
+		"branch": "it",
+		"phone": "852-795-1202",
+		"address": "P.O. Box 543, 7557 Lorem, Street",
+		"marks": 75
+	},
+	{
+		"rollno": 22,
+		"name": "Ali",
+		"age": 23,
+		"sem": 7,
+		"branch": "mech",
+		"phone": "356-841-9067",
+		"address": "3349 Dictum. Rd.",
+		"marks": 74
+	},
+	{
+		"rollno": 23,
+		"name": "Cairo",
+		"age": 18,
+		"sem": 5,
+		"branch": "ec",
+		"phone": "685-261-3554",
+		"address": "3407 Ullamcorper, St.",
+		"marks": 59
+	},
+	{
+		"rollno": 24,
+		"name": "Shelley",
+		"age": 19,
+		"sem": 3,
+		"branch": "it",
+		"phone": "764-829-8942",
+		"address": "8442 Viverra. Rd.",
+		"marks": 19
+	},
+	{
+		"rollno": 25,
+		"name": "Cairo",
+		"age": 21,
+		"sem": 8,
+		"branch": "cs",
+		"phone": "879-122-0296",
+		"address": "Ap #636-8564 Odio, Rd.",
+		"marks": 27
+	},
+	{
+		"rollno": 26,
+		"name": "Shoshana",
+		"age": 21,
+		"sem": 5,
+		"branch": "civil",
+		"phone": "179-971-4983",
+		"address": "Ap #666-4951 Non Ave",
+		"marks": 96
+	},
+	{
+		"rollno": 27,
+		"name": "Ila",
+		"age": 17,
+		"sem": 2,
+		"branch": "civil",
+		"phone": "116-314-3536",
+		"address": "769 Ipsum Rd.",
+		"marks": 50
+	},
+	{
+		"rollno": 28,
+		"name": "Quon",
+		"age": 17,
+		"sem": 1,
+		"branch": "ec",
+		"phone": "752-904-4923",
+		"address": "Ap #946-9606 Ac Avenue",
+		"marks": 56
+	},
+	{
+		"rollno": 29,
+		"name": "Nash",
+		"age": 24,
+		"sem": 3,
+		"branch": "cs",
+		"phone": "763-307-1301",
+		"address": "Ap #202-7194 Rutrum St.",
+		"marks": 91
+	},
+	{
+		"rollno": 30,
+		"name": "Hyacinth",
+		"age": 19,
+		"sem": 1,
+		"branch": "ec",
+		"phone": "356-224-9522",
+		"address": "Ap #495-1803 Mauris. Ave",
+		"marks": 96
+	},
+	{
+		"rollno": 31,
+		"name": "Herrod",
+		"age": 18,
+		"sem": 3,
+		"branch": "civil",
+		"phone": "739-279-1912",
+		"address": "P.O. Box 384, 4976 Gravida. Road",
+		"marks": 22
+	},
+	{
+		"rollno": 32,
+		"name": "Dorothy",
+		"age": 18,
+		"sem": 3,
+		"branch": "ec",
+		"phone": "687-804-1189",
+		"address": "401-5490 Vel, Rd.",
+		"marks": 87
+	},
+	{
+		"rollno": 33,
+		"name": "Colleen",
+		"age": 24,
+		"sem": 5,
+		"branch": "ec",
+		"phone": "449-124-2679",
+		"address": "240-6692 Netus Road",
+		"marks": 50
+	},
+	{
+		"rollno": 34,
+		"name": "Ali",
+		"age": 18,
+		"sem": 3,
+		"branch": "it",
+		"phone": "560-367-1235",
+		"address": "P.O. Box 954, 9335 At Rd.",
+		"marks": 52
+	},
+	{
+		"rollno": 35,
+		"name": "Kermit",
+		"age": 21,
+		"sem": 7,
+		"branch": "ec",
+		"phone": "846-703-2614",
+		"address": "441-2637 Dictum Rd.",
+		"marks": 74
+	},
+	{
+		"rollno": 36,
+		"name": "Kirestin",
+		"age": 17,
+		"sem": 1,
+		"branch": "civil",
+		"phone": "207-800-2722",
+		"address": "Ap #654-8664 Nec Ave",
+		"marks": 61
+	},
+	{
+		"rollno": 37,
+		"name": "Charity",
+		"age": 26,
+		"sem": 6,
+		"branch": "civil",
+		"phone": "847-649-0076",
+		"address": "396-8045 Id Road",
+		"marks": 63
+	},
+	{
+		"rollno": 38,
+		"name": "Ian",
+		"age": 18,
+		"sem": 5,
+		"branch": "ec",
+		"phone": "650-740-0921",
+		"address": "Ap #691-682 Ante. Street",
+		"marks": 47
+	},
+	{
+		"rollno": 39,
+		"name": "Russell",
+		"age": 18,
+		"sem": 6,
+		"branch": "mech",
+		"phone": "118-629-9925",
+		"address": "206-1321 Sem Road",
+		"marks": 95
+	},
+	{
+		"rollno": 40,
+		"name": "Jared",
+		"age": 21,
+		"sem": 1,
+		"branch": "cs",
+		"phone": "790-507-0993",
+		"address": "P.O. Box 393, 7576 Pretium Avenue",
+		"marks": 85
+	},
+	{
+		"rollno": 41,
+		"name": "Brett",
+		"age": 18,
+		"sem": 1,
+		"branch": "mech",
+		"phone": "904-917-5746",
+		"address": "P.O. Box 461, 3076 Pharetra Road",
+		"marks": 66
+	},
+	{
+		"rollno": 42,
+		"name": "Melvin",
+		"age": 21,
+		"sem": 7,
+		"branch": "mech",
+		"phone": "425-955-4824",
+		"address": "P.O. Box 799, 3526 Quisque Rd.",
+		"marks": 4
+	},
+	{
+		"rollno": 43,
+		"name": "Tara",
+		"age": 26,
+		"sem": 3,
+		"branch": "ec",
+		"phone": "501-972-1133",
+		"address": "P.O. Box 175, 9151 Posuere, Street",
+		"marks": 37
+	},
+	{
+		"rollno": 44,
+		"name": "Talon",
+		"age": 17,
+		"sem": 1,
+		"branch": "mech",
+		"phone": "588-581-5174",
+		"address": "7619 Eu St.",
+		"marks": 43
+	},
+	{
+		"rollno": 45,
+		"name": "Gretchen",
+		"age": 17,
+		"sem": 2,
+		"branch": "ec",
+		"phone": "314-311-7316",
+		"address": "Ap #288-7172 Nisl Road",
+		"marks": 79
+	},
+	{
+		"rollno": 46,
+		"name": "Hakeem",
+		"age": 20,
+		"sem": 5,
+		"branch": "cs",
+		"phone": "138-263-2706",
+		"address": "P.O. Box 210, 2013 Mauris Ave",
+		"marks": 70
+	},
+	{
+		"rollno": 47,
+		"name": "Reuben",
+		"age": 22,
+		"sem": 5,
+		"branch": "it",
+		"phone": "611-784-3965",
+		"address": "Ap #346-3624 In Ave",
+		"marks": 98
+	},
+	{
+		"rollno": 48,
+		"name": "Kiayada",
+		"age": 18,
+		"sem": 2,
+		"branch": "it",
+		"phone": "108-916-8870",
+		"address": "762 Eleifend Road",
+		"marks": 72
+	},
+	{
+		"rollno": 49,
+		"name": "Christen",
+		"age": 18,
+		"sem": 4,
+		"branch": "it",
+		"phone": "570-845-7077",
+		"address": "453-5393 Phasellus St.",
+		"marks": 22
+	},
+	{
+		"rollno": 50,
+		"name": "Zeph",
+		"age": 25,
+		"sem": 8,
+		"branch": "civil",
+		"phone": "166-778-9137",
+		"address": "938 Accumsan Av.",
+		"marks": 50
+	},
+	{
+		"rollno": 51,
+		"name": "Holmes",
+		"age": 26,
+		"sem": 3,
+		"branch": "it",
+		"phone": "351-733-0564",
+		"address": "410-812 Tellus Rd.",
+		"marks": 22
+	},
+	{
+		"rollno": 52,
+		"name": "Hunter",
+		"age": 25,
+		"sem": 6,
+		"branch": "civil",
+		"phone": "973-604-5204",
+		"address": "P.O. Box 663, 9824 Nec Road",
+		"marks": 73
+	},
+	{
+		"rollno": 53,
+		"name": "Jelani",
+		"age": 20,
+		"sem": 5,
+		"branch": "it",
+		"phone": "193-230-8788",
+		"address": "496-8229 Torquent Ave",
+		"marks": 48
+	},
+	{
+		"rollno": 54,
+		"name": "Quentin",
+		"age": 22,
+		"sem": 2,
+		"branch": "mech",
+		"phone": "313-264-1822",
+		"address": "P.O. Box 427, 636 Laoreet Avenue",
+		"marks": 81
+	},
+	{
+		"rollno": 55,
+		"name": "Oscar",
+		"age": 18,
+		"sem": 6,
+		"branch": "mech",
+		"phone": "133-566-7234",
+		"address": "7013 Venenatis Ave",
+		"marks": 19
+	},
+	{
+		"rollno": 56,
+		"name": "Faith",
+		"age": 17,
+		"sem": 8,
+		"branch": "civil",
+		"phone": "768-368-7245",
+		"address": "Ap #518-9050 Purus St.",
+		"marks": 78
+	},
+	{
+		"rollno": 57,
+		"name": "Graham",
+		"age": 17,
+		"sem": 7,
+		"branch": "mech",
+		"phone": "154-821-3819",
+		"address": "2276 Ipsum St.",
+		"marks": 79
+	},
+	{
+		"rollno": 58,
+		"name": "Jonas",
+		"age": 26,
+		"sem": 2,
+		"branch": "civil",
+		"phone": "146-352-7214",
+		"address": "Ap #831-7766 Felis. St.",
+		"marks": 73
+	},
+	{
+		"rollno": 59,
+		"name": "Sybill",
+		"age": 18,
+		"sem": 4,
+		"branch": "cs",
+		"phone": "294-664-9013",
+		"address": "P.O. Box 559, 5538 Morbi St.",
+		"marks": 56
+	},
+	{
+		"rollno": 60,
+		"name": "Phelan",
+		"age": 26,
+		"sem": 1,
+		"branch": "civil",
+		"phone": "227-311-5787",
+		"address": "4076 Donec Street",
+		"marks": 4
+	},
+	{
+		"rollno": 61,
+		"name": "Deacon",
+		"age": 18,
+		"sem": 7,
+		"branch": "civil",
+		"phone": "680-566-0545",
+		"address": "4351 Quis Rd.",
+		"marks": 68
+	},
+	{
+		"rollno": 62,
+		"name": "Hakeem",
+		"age": 19,
+		"sem": 8,
+		"branch": "civil",
+		"phone": "820-393-6916",
+		"address": "485-5758 Suspendisse Road",
+		"marks": 17
+	},
+	{
+		"rollno": 63,
+		"name": "Raven",
+		"age": 21,
+		"sem": 4,
+		"branch": "ec",
+		"phone": "537-312-6666",
+		"address": "Ap #541-8976 Ante Rd.",
+		"marks": 94
+	},
+	{
+		"rollno": 64,
+		"name": "Conan",
+		"age": 21,
+		"sem": 4,
+		"branch": "mech",
+		"phone": "136-659-7384",
+		"address": "3478 Eu, Av.",
+		"marks": 33
+	},
+	{
+		"rollno": 65,
+		"name": "Odessa",
+		"age": 17,
+		"sem": 6,
+		"branch": "it",
+		"phone": "374-504-8261",
+		"address": "5933 At Rd.",
+		"marks": 73
+	},
+	{
+		"rollno": 66,
+		"name": "Kelly",
+		"age": 19,
+		"sem": 8,
+		"branch": "civil",
+		"phone": "942-569-7552",
+		"address": "304 Fusce Ave",
+		"marks": 50
+	},
+	{
+		"rollno": 67,
+		"name": "Jescie",
+		"age": 21,
+		"sem": 7,
+		"branch": "ec",
+		"phone": "989-660-1097",
+		"address": "335-7034 Lacinia Road",
+		"marks": 95
+	},
+	{
+		"rollno": 68,
+		"name": "Carlos",
+		"age": 18,
+		"sem": 3,
+		"branch": "ec",
+		"phone": "857-624-7032",
+		"address": "690-796 Rhoncus. St.",
+		"marks": 4
+	},
+	{
+		"rollno": 69,
+		"name": "Ivor",
+		"age": 25,
+		"sem": 5,
+		"branch": "mech",
+		"phone": "574-322-8965",
+		"address": "P.O. Box 183, 1431 Nec Street",
+		"marks": 14
+	},
+	{
+		"rollno": 70,
+		"name": "Logan",
+		"age": 22,
+		"sem": 5,
+		"branch": "civil",
+		"phone": "241-495-2240",
+		"address": "1852 Nunc Road",
+		"marks": 94
+	},
+	{
+		"rollno": 71,
+		"name": "Preston",
+		"age": 18,
+		"sem": 1,
+		"branch": "it",
+		"phone": "119-108-9144",
+		"address": "Ap #873-8123 Sed Av.",
+		"marks": 3
+	},
+	{
+		"rollno": 72,
+		"name": "Allistair",
+		"age": 17,
+		"sem": 1,
+		"branch": "mech",
+		"phone": "124-223-5791",
+		"address": "404-1847 Cursus Av.",
+		"marks": 34
+	},
+	{
+		"rollno": 73,
+		"name": "Chiquita",
+		"age": 22,
+		"sem": 7,
+		"branch": "mech",
+		"phone": "530-372-2213",
+		"address": "P.O. Box 960, 4866 Orci. St.",
+		"marks": 20
+	},
+	{
+		"rollno": 74,
+		"name": "Ann",
+		"age": 21,
+		"sem": 8,
+		"branch": "mech",
+		"phone": "779-552-3735",
+		"address": "Ap #440-2186 Proin Ave",
+		"marks": 20
+	},
+	{
+		"rollno": 75,
+		"name": "Zia",
+		"age": 24,
+		"sem": 5,
+		"branch": "civil",
+		"phone": "203-423-2542",
+		"address": "131-4748 Quam Street",
+		"marks": 30
+	},
+	{
+		"rollno": 76,
+		"name": "Daryl",
+		"age": 22,
+		"sem": 8,
+		"branch": "cs",
+		"phone": "259-361-0709",
+		"address": "110-4586 Nunc Avenue",
+		"marks": 13
+	},
+	{
+		"rollno": 77,
+		"name": "Hyatt",
+		"age": 17,
+		"sem": 4,
+		"branch": "civil",
+		"phone": "634-683-7132",
+		"address": "Ap #694-3767 Congue. Av.",
+		"marks": 31
+	},
+	{
+		"rollno": 78,
+		"name": "Alden",
+		"age": 26,
+		"sem": 8,
+		"branch": "civil",
+		"phone": "209-512-7381",
+		"address": "5470 Condimentum Av.",
+		"marks": 22
+	},
+	{
+		"rollno": 79,
+		"name": "Hadley",
+		"age": 20,
+		"sem": 4,
+		"branch": "mech",
+		"phone": "475-320-2809",
+		"address": "381-5875 Sagittis. St.",
+		"marks": 57
+	},
+	{
+		"rollno": 80,
+		"name": "Phillip",
+		"age": 25,
+		"sem": 8,
+		"branch": "it",
+		"phone": "964-768-8405",
+		"address": "P.O. Box 778, 8241 Mauris Rd.",
+		"marks": 100
+	},
+	{
+		"rollno": 81,
+		"name": "Hoyt",
+		"age": 21,
+		"sem": 8,
+		"branch": "ec",
+		"phone": "768-983-9686",
+		"address": "5562 Ut, Av.",
+		"marks": 38
+	},
+	{
+		"rollno": 82,
+		"name": "Declan",
+		"age": 24,
+		"sem": 3,
+		"branch": "ec",
+		"phone": "632-709-3625",
+		"address": "6818 Eu Road",
+		"marks": 31
+	},
+	{
+		"rollno": 83,
+		"name": "Tobias",
+		"age": 20,
+		"sem": 5,
+		"branch": "mech",
+		"phone": "162-676-8416",
+		"address": "165-9818 Metus. Street",
+		"marks": 70
+	},
+	{
+		"rollno": 84,
+		"name": "Ignatius",
+		"age": 22,
+		"sem": 6,
+		"branch": "cs",
+		"phone": "723-869-7762",
+		"address": "213-1453 Ipsum. Rd.",
+		"marks": 52
+	},
+	{
+		"rollno": 85,
+		"name": "Oprah",
+		"age": 24,
+		"sem": 3,
+		"branch": "mech",
+		"phone": "393-117-1273",
+		"address": "P.O. Box 166, 6873 Magna Avenue",
+		"marks": 21
+	},
+	{
+		"rollno": 86,
+		"name": "Cherokee",
+		"age": 24,
+		"sem": 1,
+		"branch": "civil",
+		"phone": "511-758-6387",
+		"address": "919-8335 Arcu. St.",
+		"marks": 87
+	},
+	{
+		"rollno": 87,
+		"name": "Dacey",
+		"age": 21,
+		"sem": 4,
+		"branch": "it",
+		"phone": "776-428-3325",
+		"address": "455-7450 Vel Road",
+		"marks": 90
+	},
+	{
+		"rollno": 88,
+		"name": "Andrew",
+		"age": 19,
+		"sem": 2,
+		"branch": "it",
+		"phone": "652-246-2724",
+		"address": "Ap #445-8974 Quis Avenue",
+		"marks": 67
+	},
+	{
+		"rollno": 89,
+		"name": "Colin",
+		"age": 20,
+		"sem": 6,
+		"branch": "it",
+		"phone": "785-778-2456",
+		"address": "P.O. Box 176, 8858 Quis Street",
+		"marks": 76
+	},
+	{
+		"rollno": 90,
+		"name": "September",
+		"age": 20,
+		"sem": 3,
+		"branch": "cs",
+		"phone": "515-818-2680",
+		"address": "P.O. Box 904, 1452 Convallis, Ave",
+		"marks": 33
+	},
+	{
+		"rollno": 91,
+		"name": "Naomi",
+		"age": 18,
+		"sem": 3,
+		"branch": "it",
+		"phone": "749-903-6382",
+		"address": "6869 Amet Avenue",
+		"marks": 83
+	},
+	{
+		"rollno": 92,
+		"name": "Camille",
+		"age": 24,
+		"sem": 8,
+		"branch": "it",
+		"phone": "749-714-3009",
+		"address": "3581 Dictum Rd.",
+		"marks": 21
+	},
+	{
+		"rollno": 93,
+		"name": "Aiko",
+		"age": 26,
+		"sem": 4,
+		"branch": "civil",
+		"phone": "792-927-8330",
+		"address": "P.O. Box 962, 2600 Rutrum Ave",
+		"marks": 62
+	},
+	{
+		"rollno": 94,
+		"name": "Colorado",
+		"age": 26,
+		"sem": 8,
+		"branch": "it",
+		"phone": "571-259-2823",
+		"address": "9986 Sit Rd.",
+		"marks": 5
+	},
+	{
+		"rollno": 95,
+		"name": "Emerson",
+		"age": 20,
+		"sem": 6,
+		"branch": "mech",
+		"phone": "981-335-3099",
+		"address": "9295 Risus. Rd.",
+		"marks": 39
+	},
+	{
+		"rollno": 96,
+		"name": "Madison",
+		"age": 21,
+		"sem": 8,
+		"branch": "mech",
+		"phone": "294-947-5423",
+		"address": "Ap #580-6016 Diam Avenue",
+		"marks": 14
+	},
+	{
+		"rollno": 97,
+		"name": "Aquila",
+		"age": 20,
+		"sem": 2,
+		"branch": "cs",
+		"phone": "507-380-1291",
+		"address": "P.O. Box 907, 1827 Dictum Street",
+		"marks": 6
+	},
+	{
+		"rollno": 98,
+		"name": "Jessamine",
+		"age": 18,
+		"sem": 6,
+		"branch": "civil",
+		"phone": "344-971-9467",
+		"address": "P.O. Box 550, 553 Fusce Road",
+		"marks": 66
+	},
+	{
+		"rollno": 99,
+		"name": "Kareem",
+		"age": 21,
+		"sem": 3,
+		"branch": "mech",
+		"phone": "883-348-5845",
+		"address": "Ap #744-6809 In Road",
+		"marks": 94
+	},
+	{
+		"rollno": 100,
+		"name": "Ian",
+		"age": 24,
+		"sem": 7,
+		"branch": "mech",
+		"phone": "558-890-1204",
+		"address": "410-3028 Non, Rd.",
+		"marks": 48
+	}
+]
--- school.py
+++ school.py
@@ -1,22 +1,19 @@
-students = {
-    '1':{'name': 'Sreeshma', 'marks': '95', 'phonen0': '9656795252', 'email': 'sreeshma739@gmail.com'},
-    '2':{'name':'Akhil','marks':'90','phoneNO':'7403505273','Email':'akhilkannan1994@gmail.com'}}
+from data import students
 
-Action = input('Enter d to display one or m to Modify or da to Display all:')
-if Action == 'd':
-    RollNo = input('Enter the RollNo of  Student : ')
-    for val in students[RollNo] :
-        print("{} : {}".format(val, students[RollNo][val]))
-if Action == 'm' :
-    RollNo = input('Enter the RollNo of  Student : ')
-    for val in students[RollNo] :
-        print("{} : {}".format(val, students[RollNo][val]))
-    NewVar = input('Enter The New Mark : ')
-    students[RollNo]['marks'] = NewVar
-    print('The Updated Details are : ')
-    for val in students[RollNo] :
-        print("{} : {}".format(val, students[RollNo][val]))
-if Action == 'da' :
-    for RollNO in students:
-        for val in students[RollNO]:
-            print("{} : {}".format(val, students[RollNO][val]))
+found = False
+
+print('Press D for Display One student details ')
+print('Press M for Modify')
+print('Press DA for Display all Details')
+action = input()
+action = action.capitalize()
+
+if action == 'D':
+    user_rollno = int(input('Enter the RollNo : '))
+    for dict in students:
+        if dict['rollno'] == user_rollno:
+            found = True
+            for key, val in dict.items():
+                print("{} : {}".format(key, val))
+    if found == False:
+        print("Sorry, we could not find any record with the roll number you specified.")
